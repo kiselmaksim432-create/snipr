ee4f96e1edda747edf85a88f035a26655ba1f91e
kiselmaksim432-create <kiselmaksim432@gmail.com>
2026-06-01 17:31:09 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
+// src/main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // Используем HashRouter
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter> {/* <-- Уберите basename, если он там есть! */}
       <App />
     </HashRouter>
   </StrictMode>
