f927b4042c67da25fd8186496a9a7d36b4102ae7
kiselmaksim432-create <kiselmaksim432@gmail.com>
2026-06-01 16:54:56 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/** basename должно совпадать с base в vite.config.js */}
-    <BrowserRouter basename="/snipr">
+    <HashRouter basename="/snipr">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
